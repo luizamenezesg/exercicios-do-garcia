bbe60beb89d03e9078aff52265d8c9db5cf3a3bb
Luiza Menezes <142453208+luizamenezesg@users.noreply.github.com>
2024-08-16 17:52:57 -0300

    HackerRank - Exercício 1 - Simple Array Sum
    Simple Array Sum em .js

---
+++ ex1.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'simpleArraySum' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY ar as parameter.
+ */
+
+function simpleArraySum(ar) {
+    let total = 0;
+    for (let i = 0; i < ar.length; i++) { // lê número a número do array
+        total += ar[i];
+        console.log(ar[i]); // mostra cada número do array
+    }
+    return total; // retorna o total da soma
+}
+
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arCount = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = simpleArraySum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
