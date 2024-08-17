99f8089764e318d1546657c85db71b402e61bb55
Luiza Menezes <142453208+luizamenezesg@users.noreply.github.com>
2024-08-17 15:38:51 -0300

    2- Compare the Triplets

---
+++ 2- Compare the Triplets/ex2.js
@@ -0,0 +1,24 @@
+
+/*
+ * Complete the 'compareTriplets' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function compareTriplets(a, b) { //REVER NO HACKERRANK
+    let result = [0,0];    
+
+    for (let i = 0; i < 3; i++){
+        if (a [i] > b[i]){
+            result[0] += 1;
+            
+        } else if (a[i] < b[i]) {
+            result[1] += 1;            
+        }  
+                       
+    }   
+    return result; 
+}  
