f94d680a81d7f5abf077ff06a5e657cd7edeba44
Luiza Menezes <142453208+luizamenezesg@users.noreply.github.com>
2024-09-04 20:21:00 -0300

    ex9.js

--- 9- countApplesAndOranges/ex9.js
+++ 9- countApplesAndOranges/ex9.js
@@ -1,21 +1,29 @@
 function countApplesAndOranges(s, t, a, b, apples, oranges) {
     let qtdeMacas = 0;
-    let qtdeLaranjas = 0;
-          
-    for (let i = 0; i < apples.length; i++) {
-                    
-        if (apples[i] >= s && apples[i] <= t) {
+    let qtdeLaranjas = 0;  
+    let macas = [];
+    let laranjas = [];
+    
+        for (let i = 0; i < apples.length; i++) {
+            macas [i] = a + apples[i];
+        }
+    
+        for (let i = 0; i < oranges.length; i++) { 
+            laranjas [i] = b + oranges[i];
+      }      
+       
+        for (let i = 0; i < macas.length; i++) {      
+            if (macas[i] >= s && macas[i] <= t) {
             qtdeMacas++;
-        }        
-    }    
-    
-       for (let i = 0; i < oranges.length; i++) {
-  
-        if (oranges[i] >= s && oranges[i] <= t) {
+            } 
+        }             
+         
+        for (let i = 0; i < laranjas.length; i++) {          
+            if (laranjas[i] >= s && laranjas[i] <= t) {
             qtdeLaranjas++;
-        }              
-    }
-      
-    console.log (qtdeMacas, qtdeLaranjas);
-    
+        }
+      }
+              
+        console.log(qtdeMacas);
+        console.log (qtdeLaranjas);    
 }
