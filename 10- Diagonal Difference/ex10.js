450871941353826379717f1b5069767bbb1dec6e
Luiza Menezes <142453208+luizamenezesg@users.noreply.github.com>
2024-09-04 19:20:12 -0300

    ex 10 - diagonalDifference

--- 10- Diagonal Difference/ex10.js
+++ 10- Diagonal Difference/ex10.js
@@ -2,9 +2,10 @@ function diagonalDifference(arr) {
 
     let n = arr.length;
     let soma1 = arr.reduce((sum, row, index) => sum + row[index], 0);
-    let soma2 = arr.reduce((sum, row, index) => sum + row[n-1-index],0);
-    let diferenca = soma2 - soma1;
+    let soma2 = arr.reduce((sum, row, index) => sum + row[n-1-index], 0);
     
-    return (diferenca);       
+    let diferenca = Math.abs (soma1 - soma2);
+    
+    return diferenca;       
 
-}
+} 
